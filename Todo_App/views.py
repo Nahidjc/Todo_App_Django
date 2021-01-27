faafddd07645e48ea3324d3cc77c3299f65fda2c
Nahid2194 <nahidjc80@gmail.com>
2021-01-27 09:15:14 +0600

    Adding form

--- Todo_App/views.py
+++ Todo_App/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 # Create your views here.
 
 
 def index(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks/list.html', context={'tasks': tasks})
+    form = TaskForm()
+    return render(request, 'tasks/list.html', context={'tasks': tasks, 'form': form})
