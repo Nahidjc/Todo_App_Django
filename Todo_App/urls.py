36ad679e6bcf59ad944e3428587ddeff34513b99
Nahid2194 <nahidjc80@gmail.com>
2021-01-27 09:47:38 +0600

    form updated

--- Todo_App/urls.py
+++ Todo_App/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
 ]
